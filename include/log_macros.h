a9ed939764642d4d2a5db1982a585956f8824d39
Ping-Hung (Nick) Ko <ko.binghong@gmail.com>
2026-02-20 17:53:10 +0800

    Write some code to verify generated assembly
    - Studying ASM to find potential bottlenecks

--- include/log_macros.h
+++ include/log_macros.h
@@ -12,14 +12,23 @@
  */
 
 #define log_int32(first_arg) fprintf(stderr, "%s == %d\n", (#first_arg), first_arg)
-#define log_size_t(first_arg) fprintf(stderr, "%s == %zu\n", (#first_arg), first_arg)
-#define log_uint32(first_arg) fprintf(stderr, "%s == %ld\n", (#first_arg), first_arg)
+
+#define log_uint64(first_arg) fprintf(stderr, "%s == %zu\n", (#first_arg), first_arg)
+
+#define log_uint32(first_arg) fprintf(stderr, "%s == %u\n", (#first_arg), first_arg)
+
 #define log_int8(first_arg) fprintf(stderr, "%s == '%c'\n", (#first_arg), first_arg)
+
 #define log_str(first_arg) fprintf(stderr, "%s == \"%s\"\n", (#first_arg), first_arg)
+
 #define log_addr(first_arg) fprintf(stderr, "%s == %p\n", (#first_arg), (void *)first_arg)
+
 #define log_float32(first_arg) fprintf(stderr, "%s == %.016f\n", (#first_arg), first_arg)
+
 #define log_bool(first_arg)                                                              \
     fprintf(stderr, "%s == %s\n", #first_arg, (first_arg) ? "true" : "false")
+
 #define log_hex_representation(arg) fprintf(stderr, "%s == %x\n", (#arg), arg)
+
 #define log_int64(arg) fprintf(stderr, "%s == %ld\n", (#arg), arg)
 #endif /* end __LOG_MACROS_H__ */
--- main.c
+++ main.c
@@ -6,6 +6,15 @@
 // this now feels a lot more like a experiment sandbox XD
 int main(int argc, char *argv[])
 {
-    log_size_t(sizeof(short));
+    log_uint64(sizeof(short));
+    log_uint64(sizeof(int));
+    log_uint64(sizeof(long));
+
+    printf("Experimenting...");
+    float phi = PHI;
+    log_float32(phi);
+    log_hex_representation((*(unsigned int *)&phi));
+    log_uint32((*(unsigned int *)&phi));
+    log_uint64((*(unsigned long *)&phi));
     return EXIT_SUCCESS;
 }
